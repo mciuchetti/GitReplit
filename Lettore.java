0f5cd162b208cb904df4e4d32c9b9b95862fb603
mciuchetti <monica.ciuchetti@gmail.com>
2023-11-27 16:40:31 +0000

    classe Lettore iniziale

---
+++ Lettore.java
@@ -0,0 +1,11 @@
+public class Lettore{
+  String nome;
+  
+  public Lettore(String nome){
+    this.nome = nome;   
+  }
+  public void leggiCPC(String nomeFile){
+    System.out.prinln("Lettura carattere per carattere");
+    
+  }
+}
